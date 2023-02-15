5f9733d543363462a0091d19c35f4c6895c1f2f2
IvanUsatov <andrappdevelop@gmail.com>
2023-02-15 19:43:27 +0700

    edit viewmodel test, viewModel, liveData, mappers

--- app/src/main/java/com/example/numbers/numbers/domain/NumberFact.kt
+++ app/src/main/java/com/example/numbers/numbers/domain/NumberFact.kt
@@ -4,5 +4,9 @@ data class NumberFact(
     private val id: String,
     private val fact: String
 ) {
+    interface Mapper<T> {
+        fun map(id: String, fact: String): T
+    }
 
+    fun <T> map(mapper: Mapper<T>): T = mapper.map(id, fact)
 }
