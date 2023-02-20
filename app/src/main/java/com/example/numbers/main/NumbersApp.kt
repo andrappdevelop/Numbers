1c2ec0debfa51eef92cd717deabf342e3f307e72
IvanUsatov <andrappdevelop@gmail.com>
2023-02-20 19:19:38 +0700

    add cloud data source

---
+++ app/src/main/java/com/example/numbers/main/NumbersApp.kt
@@ -0,0 +1,15 @@
+package com.example.numbers.main
+
+import android.app.Application
+import com.example.numbers.BuildConfig
+import com.example.numbers.numbers.data.CloudModule
+
+
+class NumbersApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val cloudModule = if (BuildConfig.DEBUG) CloudModule.Debug() else CloudModule.Release()
+    }
+}
